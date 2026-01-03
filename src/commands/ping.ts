ce53cd22a237ea0d11c82fb7c783fbdbeec63e9e
sharkbot-neko <modoggu12345@outlook.com>
2026-01-03 21:41:00 +0900

    pingを修正

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,9 +3,14 @@ import type { Command } from '../types.js';
 export const command: Command = {
     name: 'ping',
     execute: async (note, args, stream, cli) => {
+        // Ping測定
+        const start = Date.now();
+        await cli.request("ping", {});
+        const end = Date.now();
+
         await cli.request('notes/create', {
             replyId: note.id,
-            text: `🏓Pong!`
+            text: `🏓Pong! ${end - start}ms`
         });
     }
 };
