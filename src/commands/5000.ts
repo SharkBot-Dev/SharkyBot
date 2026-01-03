59f637cd118d70ee3d00b1a8372f4f5bc9be078b
sharkbot-neko <modoggu12345@outlook.com>
2026-01-03 14:36:45 +0900

    5000兆円ほしいを追加

---
+++ src/commands/5000.ts
@@ -0,0 +1,26 @@
+import type { Command } from '../types.js';
+
+export const command: Command = {
+    name: '5000',
+    execute: async (note, args, stream, cli) => {
+        if (args.length !< 2) return;
+
+        if (!args[0]) return;
+        if (!args[1]) return;
+
+        const image = await fetch(`https://gsapi.cbrx.io/image?top=${encodeURI(args[0])}&bottom=${encodeURI(args[1])}&type=png`)
+            .then(res=>res.blob());
+
+        const file = await cli.request("drive/files/create", {
+            file: new Blob([image], { type: 'image/png' }),
+            name: `5000_${Date.now()}.png`,
+            isSensitive: true
+        });
+
+        await cli.request('notes/create', {
+            text: "5000兆円ほしい！",
+            fileIds: [file.id],
+            replyId: note.id
+        });
+    }
+};
